a69afc8afd64d5e786fd374c5133b4e2cbecc893
Emanuel Lopes <emanuel.lopes@namecheap.com>
2019-12-15 23:15:33 +0000

    Add other files

---
+++ src/main/kotlin/com/emanuelx/pontointeligente/enums/PerfilEnum.kt
@@ -0,0 +1,6 @@
+package com.emanuelx.pontointeligente.enums
+
+enum class PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/kotlin/com/emanuelx/pontointeligente/enums/TipoEnum.kt
@@ -0,0 +1,10 @@
+package com.emanuelx.pontointeligente.enums
+
+enum class TipoEnum {
+    INICIO_TRABALHO,
+    FIM_TRABALHO,
+    INICIO_ALMOCO,
+    FIM_ALMOCO,
+    INICIO_PAUSA,
+    FIM_PAUSA
+}
